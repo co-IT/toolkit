fa352cb9c7709ea525cc180042055decee85ef36
JKamue <mail@jkamue.de>
2024-08-13 10:25:37 +0200

    ci(Clockodo.QuickActions): Beim Start automatisch nach updates suchen und installieren

--- src/coIT.Clockodo.QuickActions/Program.cs
+++ src/coIT.Clockodo.QuickActions/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace coIT.Clockodo.QuickActions
+using System.Diagnostics;
+
+namespace coIT.Clockodo.QuickActions
 {
     internal static class Program
     {
@@ -8,10 +10,44 @@
         [STAThread]
         static void Main()
         {
+            var updatesWurdenGefundenUndWerdenDurchgeführt = UpdaterAktualisiertAnwendung();
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new FormMain());
+            if (!updatesWurdenGefundenUndWerdenDurchgeführt)
+            {
+                ApplicationConfiguration.Initialize();
+                Application.Run(new FormMain());
+            }
+        }
+
+        public static bool UpdaterAktualisiertAnwendung()
+        {
+            var updaterPfad = ErwarteterPfadFürUpdater();
+            if (File.Exists(updaterPfad))
+            {
+
+                Process process = Process.Start(updaterPfad);
+                process.WaitForExit();
+                var code = process.ExitCode;
+                process.Close();
+
+                // Updater exit code 0 bedeutet, dass Updates gefunden wurden
+                // https://www.advancedinstaller.com/user-guide/updater.html#section370
+                var updateGefundenExitCode = 0;
+                return code == updateGefundenExitCode;
+            }
+            return false;
+        }
+
+        public static string ErwarteterPfadFürUpdater()
+        {
+#if DEBUG
+            var appdataOrdner = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            return Path.Combine(appdataOrdner, "co-IT.eu GmbH", "Clockodo QuickActions", "updater.exe");
+#else
+            return Path.Combine(Application.StartupPath, "..", "updater.exe");
+#endif
         }
     }
 }
