468bff9d672ab681305549b93f2752c83074d1a0
JKamue <mail@jkamue.de>
2024-08-19 11:46:22 +0200

    feat(QuickActions): Hinweis zur Zwischenspeicherung von Rechnungen

--- src/coIT.Clockodo.QuickActions/Lexoffice/LexofficeRechnungskontrolle.Designer.cs
+++ src/coIT.Clockodo.QuickActions/Lexoffice/LexofficeRechnungskontrolle.Designer.cs
@@ -33,23 +33,28 @@ namespace coIT.Clockodo.QuickActions.Lexoffice
             groupBox1 = new GroupBox();
             groupBox2 = new GroupBox();
             lblErgebnisse = new Label();
+            label1 = new Label();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // tbxRechnungUrl
             // 
-            tbxRechnungUrl.Location = new Point(24, 42);
+            tbxRechnungUrl.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tbxRechnungUrl.Location = new Point(31, 74);
+            tbxRechnungUrl.Margin = new Padding(4, 5, 4, 5);
             tbxRechnungUrl.Name = "tbxRechnungUrl";
             tbxRechnungUrl.PlaceholderText = "https://app.lexoffice.de/voucher/#/########-####-####-####-############";
-            tbxRechnungUrl.Size = new Size(702, 23);
+            tbxRechnungUrl.Size = new Size(778, 30);
             tbxRechnungUrl.TabIndex = 0;
             // 
             // btnRechnungPrüfen
             // 
-            btnRechnungPrüfen.Location = new Point(732, 42);
+            btnRechnungPrüfen.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnRechnungPrüfen.Location = new Point(817, 73);
+            btnRechnungPrüfen.Margin = new Padding(4, 5, 4, 5);
             btnRechnungPrüfen.Name = "btnRechnungPrüfen";
-            btnRechnungPrüfen.Size = new Size(210, 32);
+            btnRechnungPrüfen.Size = new Size(247, 30);
             btnRechnungPrüfen.TabIndex = 1;
             btnRechnungPrüfen.Text = "Rechnung prüfen";
             btnRechnungPrüfen.UseVisualStyleBackColor = true;
@@ -57,40 +62,60 @@ namespace coIT.Clockodo.QuickActions.Lexoffice
             // 
             // groupBox1
             // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.Controls.Add(label1);
             groupBox1.Controls.Add(tbxRechnungUrl);
             groupBox1.Controls.Add(btnRechnungPrüfen);
-            groupBox1.Location = new Point(17, 31);
+            groupBox1.Location = new Point(22, 48);
+            groupBox1.Margin = new Padding(4, 5, 4, 5);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(948, 90);
+            groupBox1.Padding = new Padding(4, 5, 4, 5);
+            groupBox1.Size = new Size(1121, 138);
             groupBox1.TabIndex = 2;
             groupBox1.TabStop = false;
             groupBox1.Text = "Url der zu prüfenden Rechnung einfügen";
             // 
             // groupBox2
             // 
+            groupBox2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             groupBox2.Controls.Add(lblErgebnisse);
-            groupBox2.Location = new Point(17, 127);
+            groupBox2.Location = new Point(22, 195);
+            groupBox2.Margin = new Padding(4, 5, 4, 5);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(948, 260);
+            groupBox2.Padding = new Padding(4, 5, 4, 5);
+            groupBox2.Size = new Size(1121, 364);
             groupBox2.TabIndex = 3;
             groupBox2.TabStop = false;
             groupBox2.Text = "Ergebnisse der Prüfung";
             // 
             // lblErgebnisse
             // 
-            lblErgebnisse.Location = new Point(6, 32);
+            lblErgebnisse.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblErgebnisse.Location = new Point(8, 50);
+            lblErgebnisse.Margin = new Padding(4, 0, 4, 0);
             lblErgebnisse.Name = "lblErgebnisse";
-            lblErgebnisse.Size = new Size(936, 213);
+            lblErgebnisse.Size = new Size(1092, 282);
             lblErgebnisse.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(554, 23);
+            label1.TabIndex = 2;
+            label1.Text = "Bitte stelle sicher, dass die Rechnung zuerst zwischengespeichert wurde";
             // 
             // LexofficeRechnungskontrolle
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(9F, 23F);
             AutoScaleMode = AutoScaleMode.Font;
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
+            Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "LexofficeRechnungskontrolle";
-            Size = new Size(968, 491);
+            Size = new Size(1166, 579);
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
@@ -104,5 +129,6 @@ namespace coIT.Clockodo.QuickActions.Lexoffice
         private GroupBox groupBox1;
         private GroupBox groupBox2;
         private Label lblErgebnisse;
+        private Label label1;
     }
 }
