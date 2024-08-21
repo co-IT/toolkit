39b23183a3f6ae38b4f4b78c16872b97d8ab4dd1
JKamue <mail@jkamue.de>
2024-08-21 10:39:23 +0200

    refac(Libraries.Clockodo): Cancellationtoken optional zulassen

--- src/coIT.Libraries.Clockodo/TimeEntries/Contracts/ITimeEntryService.cs
+++ src/coIT.Libraries.Clockodo/TimeEntries/Contracts/ITimeEntryService.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections.Immutable;
+using System.Collections.Immutable;
 
 namespace coIT.Libraries.Clockodo.TimeEntries.Contracts;
 
 public interface ITimeEntriesService
 {
-    Task<IImmutableList<TimeEntry>> GetTimeEntriesAsync(ClockodoPeriod period, int employee);
+    Task<IImmutableList<TimeEntry>> GetTimeEntriesAsync(
+        ClockodoPeriod period,
+        int employee,
+        CancellationToken cancellationToken
+    );
 }
