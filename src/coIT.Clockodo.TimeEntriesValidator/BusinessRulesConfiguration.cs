8ef7208b44439578e7555918b64a2c23c291df49
JKamue <mail@jkamue.de>
2024-08-26 09:31:49 +0200

    choe(Clockodo.TimeEntriesValidator): Aus Toolkit Repo entfernen

--- src/coIT.Clockodo.TimeEntriesValidator/BusinessRulesConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace coIT.Clockodo.TimeEntriesValidator
-{
-    internal class BusinessRulesConfiguration
-    {
-        public Dictionary<string, List<string>> ServiceRules { get; set; }
-        public Dictionary<string, string> TextRules { get; set; }
-    }
-}
